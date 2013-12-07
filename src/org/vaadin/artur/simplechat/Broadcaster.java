99d456c0fa9b523daab57c504a02c677ba265f4e
Artur Signell <artur@vaadin.com>
2013-12-07 11:54:51 +0200

    Combined into one UI class again. Added color coding depending on UI id

--- src/org/vaadin/artur/simplechat/Broadcaster.java
+++ src/org/vaadin/artur/simplechat/Broadcaster.java
@@ -5,12 +5,16 @@ import java.util.EventObject;
 
 import org.vaadin.artur.simplechat.Broadcaster.MessageListener.MessageEvent;
 
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.event.EventRouter;
 import com.vaadin.util.ReflectTools;
 
 public class Broadcaster {
 
 	private static Broadcaster instance = new Broadcaster();
+
+	private static String[] colors = new String[] { "red", "green", "blue",
+			"magenta", "black" };
 
 	private EventRouter router = new EventRouter();
 
@@ -20,14 +24,20 @@ public class Broadcaster {
 
 		public static class MessageEvent extends EventObject {
 			String message;
+			private String senderColor;
 
-			public MessageEvent(String message) {
+			public MessageEvent(String senderColor, String message) {
 				super(new Object());
 				this.message = message;
+				this.senderColor = senderColor;
 			}
 
 			public String getMessage() {
 				return message;
+			}
+
+			public String getSenderColor() {
+				return senderColor;
 			}
 
 		}
@@ -35,8 +45,12 @@ public class Broadcaster {
 		public void messageReceived(MessageEvent messageEvent);
 	}
 
-	public static void sendMessage(String message) {
-		instance.router.fireEvent(new MessageEvent(message));
+	public static void sendMessage(String message, SimpleChatUI source) {
+		instance.router.fireEvent(new MessageEvent(getColor(source), message));
+	}
+
+	private static String getColor(SimpleChatUI source) {
+		return colors[source.getUIId() % 5];
 	}
 
 	public static void addMessageListener(MessageListener messageListener) {
--- src/org/vaadin/artur/simplechat/SimpleChatUI.java
+++ src/org/vaadin/artur/simplechat/SimpleChatUI.java
@@ -6,7 +6,10 @@ import java.util.Locale;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.vaadin.artur.simplechat.Broadcaster.MessageListener;
+
 import com.vaadin.annotations.Push;
+import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
@@ -16,10 +19,17 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.UIDetachedException;
 import com.vaadin.ui.VerticalLayout;
 
 @Push
+@Theme("SimpleChat")
 public class SimpleChatUI extends UI {
+
+	private Panel chatPanel;
+	private VerticalLayout chatLog;
+	private TextField chatMessage;
+	private VerticalLayout mainLayout;
 
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = SimpleChatUI.class)
@@ -28,6 +38,72 @@ public class SimpleChatUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-		setContent(new SimpleChat());
+		createUI();
+		setupLogic();
+		setLocale(Locale.ENGLISH);
+	}
+
+	private void createUI() {
+		mainLayout = new VerticalLayout();
+		mainLayout.setSizeFull();
+		mainLayout.setMargin(true);
+
+		chatPanel = new Panel("Chat");
+		chatPanel.setSizeFull();
+
+		chatLog = new VerticalLayout();
+		chatLog.setMargin(true);
+		chatLog.setWidth("100%");
+		chatLog.setHeight(null);
+		chatPanel.setContent(chatLog);
+
+		chatMessage = new TextField();
+		chatMessage.setImmediate(true);
+		chatMessage.setWidth("100%");
+		chatMessage.setInputPrompt("Write your message and press enter");
+
+		mainLayout.addComponent(chatPanel);
+		mainLayout.setExpandRatio(chatPanel, 1);
+		mainLayout.addComponent(chatMessage);
+
+		setContent(mainLayout);
+	}
+
+	private void setupLogic() {
+		chatMessage.addValueChangeListener(new ValueChangeListener() {
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				TextField field = (TextField) event.getProperty();
+				if (!"".equals(field.getValue())) {
+					Broadcaster.sendMessage(field.getValue(), SimpleChatUI.this);
+					field.setValue("");
+				}
+			}
+		});
+
+		Broadcaster.addMessageListener(new MessageListener() {
+			@Override
+			public void messageReceived(final MessageEvent event) {
+				try {
+					getUI().access(new Runnable() {
+						@Override
+						public void run() {
+							DateFormat df = DateFormat
+									.getTimeInstance(DateFormat.MEDIUM);
+							Label messageLabel = new Label(df
+									.format(new Date())
+									+ ": "
+									+ event.getMessage());
+							messageLabel.setStyleName("chat-"
+									+ event.getSenderColor());
+							chatLog.addComponent(messageLabel);
+						}
+					});
+				} catch (UIDetachedException e) {
+					Broadcaster.removeMessageListener(this);
+				}
+			}
+		});
+
 	}
 }
