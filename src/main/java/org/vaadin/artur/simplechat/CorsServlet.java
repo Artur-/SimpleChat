3d24e3166b668a177bfb9a354b38e0631eb05972
Artur Signell <artur@vaadin.com>
2019-05-23 13:46:56 +0300

    Simplify CORS servlet

--- src/main/java/org/vaadin/artur/simplechat/CorsServlet.java
+++ src/main/java/org/vaadin/artur/simplechat/CorsServlet.java
@@ -1,60 +1,77 @@
 package org.vaadin.artur.simplechat;
 
 import java.io.IOException;
-import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.vaadin.flow.server.ServletHelper;
 import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.server.VaadinServletRequest;
+import com.vaadin.flow.server.ServletHelper.RequestType;
 
-@WebServlet(value = "/*", asyncSupported = true, initParams = {
-        @WebInitParam(name = "pushURL", value = "//artur.app.fi/simplechat-v14/") })
+@WebServlet(value = "/*", asyncSupported = true)
 public class CorsServlet extends VaadinServlet {
+    private Set<String> allowedOrigins = new HashSet<>();
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+
+        // "*" allows ALL WEB SITES to embed components from this application
+        allowedOrigins.add("*");
+    }
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String origin = request.getHeader("Origin");
-        if (request.getRequestURI().startsWith("/web-component/")) {
+        if (needsCorsHeaders(request) && isAllowedRequestOrigin(origin)) {
+            System.out.println("CORS request from " + origin + " for " + request.getPathInfo());
+            response.addHeader("Access-Control-Allow-Origin", origin);
+            response.addHeader("Access-Control-Allow-Credentials", "true");
 
-            if (origin != null && isAllowedRequestOrigin(origin)) {
-                // Handle a preflight "option" requests
-                if ("options".equalsIgnoreCase(request.getMethod())) {
-                    response.addHeader("Access-Control-Allow-Origin", origin);
-                    response.setHeader("Allow", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS");
+            // Handle a preflight "option" requests
+            if ("options".equalsIgnoreCase(request.getMethod())) {
+                response.addHeader("Access-Control-Allow-Methods", "GET, POST");
+                response.addHeader("Access-Control-Allow-Headers", "content-type");
 
-                    // allow the requested method
-                    String method = request.getHeader("Access-Control-Request-Method");
-                    response.addHeader("Access-Control-Allow-Methods", method);
-
-                    // allow the requested headers
-                    String headers = request.getHeader("Access-Control-Request-Headers");
-                    response.addHeader("Access-Control-Allow-Headers", headers);
-
-                    response.addHeader("Access-Control-Allow-Credentials", "true");
-                    response.setContentType("text/plain");
-                    response.setCharacterEncoding("utf-8");
-                    response.getWriter().flush();
-                    return;
-                } // Handle UIDL post requests
-                else {
-                    response.addHeader("Access-Control-Allow-Origin", origin);
-                    response.addHeader("Access-Control-Allow-Credentials", "true");
-                    super.service(request, response);
-                    return;
-                }
+                response.setContentType("text/plain");
+                response.setCharacterEncoding("utf-8");
+                response.getWriter().flush();
+                return;
+            } else {
+                response.addHeader("Vary", "Origin");
             }
+
         }
         super.service(request, response);
+    }
 
+    private boolean needsCorsHeaders(HttpServletRequest request) {
+        VaadinServletRequest vaadinRequest = new VaadinServletRequest(request, getService());
+        String path = vaadinRequest.getPathInfo();
+        if (ServletHelper.isRequestType(vaadinRequest, RequestType.UIDL)) {
+            return true;
+        } else if (path.startsWith("/build/")) {
+            return true;
+        }
+        return false;
     }
 
     private boolean isAllowedRequestOrigin(String origin) {
-        // TODO: Remember to limit the origins.
-        return origin.matches(".*");
+        System.out.println("Checking if origin is ok: " + origin);
+        if (origin == null)
+            return false;
+        if (allowedOrigins.contains("*")) {
+            return true;
+        }
+        return allowedOrigins.contains(origin);
     }
 
 }
